ae7c3790b9427af6986afaffcc78dfed860ce6ab
Pankaj Kumar <pk919284@gmail.com>
2025-09-11 15:01:23 +0530

    chore: Set final production URL

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import { Download, Users, CheckCircle, Clock, Mail, User, BarChart3, Activity, S
 
 // --- API and Socket Configuration ---
 // Paste your Render URL here
-const API_URL = 'https://event-attendance-system-loum.onrender.com/api';
-const SOCKET_URL = 'https://event-attendance-system-loum.onrender.com';
+const API_URL = 'https://event-attendance-system-2rpq.onrender.com/api';
+const SOCKET_URL = 'https://event-attendance-system-2rpq.onrender.com';
 const socket = io(SOCKET_URL);
 
 // --- The Main Application Component ---
